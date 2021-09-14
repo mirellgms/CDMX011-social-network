4c290f18685fbf97b35534ee8c4bd89bd9006202
GeraldineEJ <geraldineestrada29@gmail.com>
2021-09-14 14:18:29 -0500

    seleccionar categoria de post

--- src/Components/feed.js
+++ src/Components/feed.js
@@ -2,6 +2,7 @@
 // eslint-disable-next-line import/no-cycle
 // import { onNavigate } from '../main.js';
 import { logout } from '../lib/firebase.js';
+// import db from './secret.js';
 
 export const Feed = () => {
   const feedDiv = document.createElement('div');
@@ -10,6 +11,33 @@ export const Feed = () => {
   const title = document.createElement('img');
   title.setAttribute('src', '../img/be-the-light.png');
   title.id = 'title';
+
+  let option = document.createElement('select');
+
+  let category = document.createElement('option');
+  category.setAttribute('value', 'Select');
+  let categoryText = document.createTextNode('Selecciona una categoría de tu post');
+  category.appendChild(categoryText);
+
+  let devocional = document.createElement('option');
+  devocional.setAttribute('value', 'devocional');
+  let devocionalText = document.createTextNode('Devocional');
+  devocional.appendChild(devocionalText);
+
+  let estudioBiblico = document.createElement('option');
+  estudioBiblico.setAttribute('value', 'estudioBiblico');
+  let estudioBiblicoText = document.createTextNode('Estudio Bíblico');
+  estudioBiblico.appendChild(estudioBiblicoText);
+
+  let  musica= document.createElement('option');
+  musica.setAttribute('value', 'musica');
+  let musicaText = document.createTextNode('Música');
+  musica.appendChild(musicaText);
+
+  let eventos = document.createElement('option');
+  eventos.setAttribute('value', 'eventos');
+  let eventosText = document.createTextNode('Eventos');
+  eventos.appendChild(eventosText);
 
   const post = document.createElement('input');
   post.placeholder = '¿Qué estas pensando?';
@@ -27,7 +55,15 @@ export const Feed = () => {
   buttonLogout.addEventListener('click', () => {
     logout();
   });
+
+
   feedDiv.appendChild(title);
+  feedDiv.appendChild(option);
+  option.appendChild(category);
+  option.appendChild(devocional);
+  option.appendChild(estudioBiblico);
+  option.appendChild(musica);
+  option.appendChild(eventos);
   feedDiv.appendChild(post);
   feedDiv.appendChild(publish);
   feedDiv.appendChild(buttonLogout);
