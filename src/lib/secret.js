cd3282c380f797bf209c82c7ca8738d2dbc3c54e
mirellgms <mirellgmsmkt@gmail.com>
2021-10-15 23:02:32 -0500

    secret

---
+++ src/lib/secret.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCZeNUhg_VRvifG6XjBkhHwYqWpWmEFhO8',
+  authDomain: 'bethelight-e8453.firebaseapp.com',
+  projectId: 'bethelight-e8453',
+  storageBucket: 'bethelight-e8453.appspot.com',
+  messagingSenderId: '667917132227',
+  appId: '1:667917132227:web:efa4d4cb9c9182efc09d76',
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase;
